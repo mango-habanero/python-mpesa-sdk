cf7c5fc99cae4a2af4c3ce484f451d504e80deda
mango-habanero <main@mango-habanero.dev>
2023-06-18 12:07:57 +0300

    fix(test): Make ".env.test" file available for tests to access environment variables.

--- tests/fixtures/base.py
+++ tests/fixtures/base.py
@@ -1,8 +1,15 @@
+# standard imports
 import os
 
+# external imports
 import dotenv
 import pytest
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+env_file = os.path.join(parent_dir, '.env.test')
+
+
 @pytest.fixture(scope='session')
 def load_env_vars():
-    dotenv.load_dotenv()
+    dotenv.load_dotenv(dotenv_path=env_file)
